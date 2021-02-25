93870c9e24d89724b7e5a0c8874d9ff98571279c
Colin McGregor <cmcgregor@live.ca>
2021-02-25 09:49:42 -0500

    Menus Added
    Menus Added

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,25 +6,66 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     GameObject[] gameOverObjects;
+    GameObject[] pauseMenuObjects;
     PlayerController player;
 
     void Start()
     {
         Time.timeScale = 1;
+        
         gameOverObjects = GameObject.FindGameObjectsWithTag("Game_Over");
+        pauseMenuObjects = GameObject.FindGameObjectsWithTag("Pause_Menu");
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-        HideGameOver();
+        
+        HidePauseMenu();
+        HideGameOverMenu();
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Time.timeScale == 1)
+            {
+                Time.timeScale = 0;
+                ShowPauseMenu();
+            }
+            else if (Time.timeScale == 0)
+            {
+                Time.timeScale = 1;
+                HidePauseMenu();
+            }
+        }
+
         if (Time.timeScale == 0 && !player.isAlive)
         {
-            ShowGameOver();
+            ShowGameOverMenu();
         }
     }
 
-    public void ShowGameOver()
+    public void PlayButton()
+    {
+        Time.timeScale = 1;
+        HidePauseMenu();
+    }
+
+    public void ShowPauseMenu()
+    {
+        foreach (GameObject item in pauseMenuObjects)
+        {
+            item.SetActive(true);
+        }
+    }
+
+    public void HidePauseMenu()
+    {
+        foreach (GameObject item in pauseMenuObjects)
+        {
+            item.SetActive(false);
+        }
+    }
+
+    public void ShowGameOverMenu()
     {
         foreach (GameObject item in gameOverObjects)
         {
@@ -32,7 +73,7 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void HideGameOver()
+    public void HideGameOverMenu()
     {
         foreach (GameObject item in gameOverObjects)
         {
